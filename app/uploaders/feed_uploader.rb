b25b5a581394b5bfcc56464739205ac072bc855b
ryosuke takasawa <ten.r0619@gmail.com>
2020-03-18 20:35:51 +0900

    フロントデザインを修正した

--- app/uploaders/feed_uploader.rb
+++ app/uploaders/feed_uploader.rb
@@ -1,7 +1,7 @@
 class FeedUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
-  process :resize_to_limit => [50,50]
+  process :resize_to_limit => [400,400]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
